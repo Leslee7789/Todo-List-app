e4b20333b0f12412e442b7fc5782f9ec93af4fa0
Leslee7789 <lesliefleischer8@gmail.com>
2026-03-22 14:58:17 -0700

    implement add task function

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputField = document.getElementById('input-field');
+const listContainer = document.getElementById('list-container');
+
+function addTask() {
+    if(inputField.value === '') {
+        alert('Please enter a task');
+    }else {
+        let li = document.createElement('li');
+        li.innerHTML = inputField.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    inputField.value = '';
+}
